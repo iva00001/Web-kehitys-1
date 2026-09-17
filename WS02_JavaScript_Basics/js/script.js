0704cdcc7a99cb45daee97635fd0745abe530d4a
Ivona Aksamentova <Ivona.Aksamentova@student.laurea.fi>
2026-09-17 19:39:21 +0300

    almost

---
+++ WS02_JavaScript_Basics/js/script.js
@@ -0,0 +1,34 @@
+console.log("Hello, World!");
+
+const userName = "Ivona";
+const favoriteAnimal = "Snow Leopard";
+const userAge = 18;
+
+console.log("My name is " + userName);
+console.log("My favorite animal is " + favoriteAnimal);
+console.log("My age is " + userAge);
+
+alert("Tervetuloa hieoulle nettisivylleni!");
+
+const visitorName = prompt("Mikä on nimesi?");
+console.log(visitorName);
+console.log("Hei " + visitorName + "! Tervetuloa JavaScriptin pariin!");
+
+const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?");
+console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
+
+const visitorAge = prompt("Kuinka vanha olet?");
+
+if (visitorAge >= 18) {
+  console.log("Olet aikuinen.");
+} else {
+  console.log("Olet alle 18-vuotias.");
+}
+
+function greetUser(name) {
+  console.log("Hei " + name + "!");
+}
+
+greetUser("Ivona");
+greetUser('Lidia');
+greetUser('Ekaterina');
